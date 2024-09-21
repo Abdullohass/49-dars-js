3d4e0f2709efed7e298f9f42c13e64f1dbd69727
Abdullohass <abdulloh.s.gk@gmail.com>
2024-09-21 15:28:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const mevalar = ["uzum", "olma", "nok", "hurmo",]
+alert("Boshidagi array lengthi: " + mevalar.length);
+console.log(mevalar);
+
+let javob = confirm("Biz arrey ichidan 1 ta mevani ochirmoqchimiz")
+if (javob) {
+    mevalar.pop()
+} else {
+
+}
+alert("Array length: " + mevalar.length)
+console.log(mevalar);
+
+
+
+
+
+
